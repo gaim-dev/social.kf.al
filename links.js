cb037a2c75861282ec264f1b02600fe3cb5eb47b
Benjamin Altpeter <benjamin@oldpeter.de>
2018-12-08 17:05:51 +0100

    Load links dynamically

---
+++ links.js
@@ -0,0 +1,11 @@
+const LINKS = [
+	['Datenschutzerklärung Instagram', 'https://img.kfcdn.de/other/docs/instagram-privacy.html'],
+	['Webseite', 'https://kochfokus.de'],
+	['YouTube', 'https://www.youtube.com/user/kochfokusTV/'],
+	['Facebook', 'https://www.facebook.com/kochfokusDE/'],
+	['Twitter', 'https://twitter.com/kochfokusDE/'],
+	['Google+', 'https://plus.google.com/+kochfokusDE/'],
+	['Instagram', 'https://instagram.com/kochfokus.de/'],
+	['Pinterest', 'https://www.pinterest.com/kochfokus/'],
+	['Flipboard', 'https://flipboard.com/@kochfokusde/']
+];
