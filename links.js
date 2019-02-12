4e6b472a5f6da90f36b2ecbe8005c930b9679d2d
GabiAlt <45716248+GabiAlt@users.noreply.github.com>
2019-02-12 17:39:43 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 const LINKS = [
-	['Super leckerer Chiapudding mit Himbeerpüree und Schokoladencreme', 'https://kochfokus.de/artikel/super-leckerer-chiapudding-mit-himbeerpueree-und-schokoladencreme/'],
+	['Bretonischer Aprikosen-Far', 'https://kochfokus.de/artikel/bretonischer-aprikosen-far/'],
 	['Datenschutzerklärung Instagram', 'https://img.kfcdn.de/other/docs/instagram-privacy.html'],
 	['Webseite', 'https://kochfokus.de'],
 	['YouTube', 'https://www.youtube.com/user/kochfokusTV/'],
