cc9bdce502e1d16650586c092be435a79cdedf94
GabiAlt <45716248+GabiAlt@users.noreply.github.com>
2019-02-22 17:56:49 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 const LINKS = [
-	['Gabis Frittata mit Zucchini und Tomaten', 'https://kochfokus.de/artikel/gabis-frittata-mit-zucchini-und-tomaten/'],
+	['Superfoodbrötchen', 'https://kochfokus.de/artikel/superfoodbroetchen-chiasamen-protein/'],
 	['Datenschutzerklärung Instagram', 'https://img.kfcdn.de/other/docs/instagram-privacy.html'],
 	['Webseite', 'https://kochfokus.de'],
 	['YouTube', 'https://www.youtube.com/user/kochfokusTV/'],
