ded3b2bd9bd3ae6b5779cfd2cf0184296d10ed54
GabiAlt <45716248+GabiAlt@users.noreply.github.com>
2019-04-18 12:01:47 +0200

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 const LINKS = [
-	['Super fruchtiges Kastenbrot mit Chia Samen', 'https://kochfokus.de/artikel/super-fruchtiges-kastenbrot-mit-chia-samen/'],
+	['Schokoladen Orangen Cookies', 'https://kochfokus.de/artikel/schokoladen-orangen-cookies/'],
 	['Datenschutzerklärung Instagram', 'https://img.kfcdn.de/other/docs/instagram-privacy.html'],
 	['Webseite', 'https://kochfokus.de'],
 	['YouTube', 'https://www.youtube.com/user/kochfokusTV/'],
