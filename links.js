3e6fadce30ab9ccbc54115a7e552ea2f2141c014
GabiAlt <45716248+GabiAlt@users.noreply.github.com>
2019-06-01 17:19:42 +0200

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 const LINKS = [
-	['Herrlich erfrischender Sommersalat mit Melonen', 'https://kochfokus.de/artikel/herrlich-erfrischendender-sommersalat-mit-melonen/'],
+	['Avocado Zitronenmuffins mit Schokoladenstücken', 'https://kochfokus.de/artikel/avocado-zitronenmuffins-mit-schokoladenstuecken/'],
 	['Datenschutzerklärung Instagram', 'https://img.kfcdn.de/other/docs/instagram-privacy.html'],
 	['Webseite', 'https://kochfokus.de'],
 	['YouTube', 'https://www.youtube.com/user/kochfokusTV/'],
