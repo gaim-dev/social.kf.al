2e584c21a94e9f3bab93086b10353d33bbb75f54
GabiAlt <45716248+GabiAlt@users.noreply.github.com>
2019-10-06 21:58:03 +0200

    Update links.js

--- links.js
+++ links.js
@@ -2,6 +2,7 @@ const LINKS = [
 	['Rezept Hokkaido Kürbissuppe | einfach und gut', 'https://kochfokus.de/artikel/hokkaido-kuerbis-suppe/'],
 	['Dieser Schokoladenbananenkuchen rockt!', 'https://kochfokus.de/artikel/dieser-schokoladen-bananenkuchen-rockt-kochfokus-de/'],
 	['Apfel-Haselnuss-Kürbiskuchen | himmlisch lecker', 'https://kochfokus.de/artikel/apfel-haselnuss-kuerbiskuchen-himmlisch-lecker/'],
+	['Hallo! Ich bin Gabi', 'https://kochfokus.de/about-me/'],
 	['Datenschutzerklärung Instagram', 'https://img.kfcdn.de/other/docs/instagram-privacy.html'],
 	['Webseite', 'https://kochfokus.de'],
 	['YouTube', 'https://www.youtube.com/user/kochfokusTV/'],
