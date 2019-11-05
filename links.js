eb311418f7e40dc437ab8ad38260f32cfbeae852
GabiAlt <45716248+GabiAlt@users.noreply.github.com>
2019-11-05 16:14:59 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,9 +1,9 @@
 const LINKS = [
+	['Zitronenkuchen mit Mohn | einfach, lecker & fluffig', 'https://kochfokus.de/artikel/zitronenkuchen-mit-mohn-einfach-lecker-und-fluffig/'],
 	['Rezept Low Carb Schokoladenkekse', 'https://kochfokus.de/artikel/rezept-low-carb-schokoladenkekse/'],
 	['Herbstliches Knäckebrot', 'https://kochfokus.de/artikel/herbstliches-knaeckebrot/'],
 	['Gelingsichere Frühstücksmuffins mit Cranberries', 'https://kochfokus.de/artikel/fruehstuecksmuffins-mit-cranberries/'],
 	['Haferflockenbrot mit knuspriger Kruste und fluffiger Krume', 'https://kochfokus.de/artikel/haferflockenbrot-mit-knuspriger-kruste-und-fluffiger-krume/'],
-	['Frühstückskuchen', 'https://kochfokus.de/artikel/fruehstuecks-kuchen/'],
 	['Hallo! Ich bin Gabi', 'https://kochfokus.de/about-me/'],
 	['Datenschutzerklärung Instagram', 'https://img.kfcdn.de/other/docs/instagram-privacy.html'],
 	['Webseite', 'https://kochfokus.de'],
